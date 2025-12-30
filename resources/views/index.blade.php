ecc774ef28019c9b7815997caf7bfdc87e4f2472
Philip-11 <magsaysayphil@gmail.com>
2025-12-30 23:54:03 +0800

    Added login route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
             <h3 class="text-8xl">Welcome to <br> <strong>Ubae❣ <br> Recipes</strong></h3>
         </div>
         <div class="content-center p-6">
-            <button class="btn btn-xl btn-outline">Add fun to your purple yam!</button>
+            <a href="/login" class="btn btn-xl btn-outline">Add fun to your purple yam!</a>
         </div>
     </section>
 </x-layout>

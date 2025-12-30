fa33769cb360b523ca0d534f66d3d43f06c9b881
Philip-11 <magsaysayphil@gmail.com>
2025-12-31 02:34:11 +0800

    Created recipe card component

---
+++ resources/views/components/recipe-card.blade.php
@@ -0,0 +1,14 @@
+<div class="card bg-base-100 w-96 shadow-sm">
+    <figure>
+    <img
+        src="https://www.kawalingpinoy.com/wp-content/uploads/2019/09/ube-halaya-3-730x973.jpg"
+        alt="Shoes"/>
+    </figure>
+    <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+    </div>
+</div>
+</div>

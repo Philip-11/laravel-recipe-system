0b0d1b27c396823bba9f97bcfbcfa07d35c998e0
Philip-11 <magsaysayphil@gmail.com>
2025-12-30 11:25:15 +0800

    now uses the layout

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar shadow-sm">
     <div class="flex-1">
-        <a href="#" class="text-xl btn btn-ghost"><strong> Ube Recipes</strong></a>
+        <a href="#" class="text-xl btn btn-ghost"><strong> Ubae❣ Recipes</strong></a>
     </div>
     <div class="flex-none">
         <a href="#" class="text-lg btn btn-ghost">Login</a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="m-3 p-3">
-    <header class="m-3">
-        <x-navigation/>
-    </header>
-    <br>
-    <main>
-        <section class="flex m-3 pt-3 shadow-md">
-            <div class="flex-2 p-6">
-                <h3 class="text-8xl">Welcome to <br> <strong>Ube <br> Recipes</strong></h3>
-            </div>
-            <div class="content-center p-6">
-                <button class="btn btn-xl btn-outline">Add fun to your purple yam!</button>
-            </div>
-        </section>
-    </main>
-</body>
-</html> 
+<x-layout>
+    <section class="flex m-3 pt-3 shadow-md">
+        <div class="flex-2 p-6">
+            <h3 class="text-8xl">Welcome to <br> <strong>Ubae❣ <br> Recipes</strong></h3>
+        </div>
+        <div class="content-center p-6">
+            <button class="btn btn-xl btn-outline">Add fun to your purple yam!</button>
+        </div>
+    </section>
+</x-layout>

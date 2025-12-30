94562a2ca65ba9fa506bec0b517d5d242602b58f
Philip-11 <magsaysayphil@gmail.com>
2025-12-30 20:52:19 +0800

    added route

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar shadow-sm">
     <div class="flex-1">
-        <a href="" class="text-xl btn btn-ghost"><strong> Ubae❣ Recipes</strong></a>
+        <a href="/" class="text-xl btn btn-ghost"><strong> Ubae❣ Recipes</strong></a>
     </div>
     <div class="flex-none">
         <a href="/login" class="text-lg btn btn-ghost">Login</a>

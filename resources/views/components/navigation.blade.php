abe2e74ca44b9a2616e25019b33b3cc8dae039fe
Philip-11 <magsaysayphil@gmail.com>
2025-12-30 10:51:14 +0800

    Initial Commit

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar shadow-sm">
+    <div class="flex-1">
+        <a href="#" class="text-xl btn btn-ghost"><strong> Ube Recipes</strong></a>
+    </div>
+    <div class="flex-none">
+        <a href="#" class="text-lg btn btn-ghost">Login</a>
+    </div>
+</nav>

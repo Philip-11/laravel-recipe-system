07fc07e2b06052855ecedad56d915d680491f821
Philip-11 <magsaysayphil@gmail.com>
2025-12-31 02:35:05 +0800

    Auth controller

--- resources/views/components/recipe-card.blade.php
+++ resources/views/components/recipe-card.blade.php
@@ -1,14 +1,14 @@
 <div class="card bg-base-100 w-96 shadow-sm">
     <figure>
     <img
-        src="https://www.kawalingpinoy.com/wp-content/uploads/2019/09/ube-halaya-3-730x973.jpg"
-        alt="Shoes"/>
+        src="{{ $image }}"
+        alt="{{ $alt }}"/>
     </figure>
     <div class="card-body">
-    <h2 class="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <h2 class="card-title">{{ $title }}</h2>
+    <p>{{ $description }}</p>
     <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+        <button class="btn btn-primary">View</button>
     </div>
 </div>
 </div>

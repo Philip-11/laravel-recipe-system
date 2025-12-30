a6ab9cdb5d8b4648952f818286ccceb3be42cc2e
Philip-11 <magsaysayphil@gmail.com>
2025-12-30 18:59:43 +0800

    Added login route

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar shadow-sm">
     <div class="flex-1">
-        <a href="#" class="text-xl btn btn-ghost"><strong> Ubae❣ Recipes</strong></a>
+        <a href="" class="text-xl btn btn-ghost"><strong> Ubae❣ Recipes</strong></a>
     </div>
     <div class="flex-none">
-        <a href="#" class="text-lg btn btn-ghost">Login</a>
+        <a href="/login" class="text-lg btn btn-ghost">Login</a>
     </div>
 </nav>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <form action="" method="POST">
+        @csrf
+
+        <x-text-input name="email" label="Email Address" type="email" required/>
+    </form>
+</x-layout>

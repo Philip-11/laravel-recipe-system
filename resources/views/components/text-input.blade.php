8ed1072d84691def787efe75cca112c8ebad4ca6
Philip-11 <magsaysayphil@gmail.com>
2025-12-30 20:12:42 +0800

    Created login page

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,11 +1,14 @@
+@props(['name', 'label', 'type'])
 <div class="form-group">
-    <label for="{{ $name }}">{{ $label }}</label>
+    <label for="{{ $name }}" class="label">{{ $label }} {{ $attributes->has('required') ? '*' : '' }}</label>
 
     <input type="text"
         type="{{ $type }}"
         name="{{ $name }}"
         id="{{ $name }}"
         value="{{ old($name) }}"
+        class="input"
+        {{ $attributes }}
     >
 
     @error($name)
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,13 @@
 <x-layout>
-    <form action="" method="POST">
-        @csrf
+    <section>
+        <fieldset class="fieldset bg-base-100 rounded-box w-xs">
+            <form action="" method="POST">
+                @csrf
 
-        <x-text-input name="email" label="Email Address" type="email" required/>
-    </form>
+                <x-text-input name="email" label="Email Address" type="email" required/>
+                <x-text-input name="password" label="Password" type="password" required/>
+                <button type="submit" class="btn btn-neutral mt-3">Login</button>
+            </form>
+        </fieldset>
+    </section>
 </x-layout>

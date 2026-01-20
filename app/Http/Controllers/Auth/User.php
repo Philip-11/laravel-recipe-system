50c301ee987168dfa223975480e9873aa6db7399
Philip-11 <magsaysayphil@gmail.com>
2026-01-20 11:17:45 +0800

    login and logout function

--- app/Http/Controllers/Auth/User.php
+++ app/Http/Controllers/Auth/User.php
@@ -4,11 +4,32 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Controller
 {
     public function login(Request $request)
     {
-        
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'password']
+        ]);
+
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard', 200);
+        }
+
+        return back()->withErrors(['email' => 'Invalid Credentials']);
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
